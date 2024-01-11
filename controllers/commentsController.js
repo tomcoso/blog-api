9e44be6eb93c4151b5470f9e3cc7e1f986d010b6
tomcoso <tomasdessy0@gmail.com>
2024-01-11 18:58:57 -0300

    add comment create and delete controller

--- controllers/commentsController.js
+++ controllers/commentsController.js
@@ -1,5 +1,41 @@
 const asyncHandler = require("express-async-handler");
+const { body, validationResult } = require("express-validator");
+const passport = require("passport");
 
-exports.comments_post = asyncHandler(async (req, res, next) => {});
+const Comment = require("../models/comments");
 
-exports.comments_delete = asyncHandler(async (req, res, next) => {});
+exports.comments_post = [
+  body("username")
+    .trim()
+    .isLength({ min: 2, max: 20 })
+    .withMessage("username must be between 2 and 20 characters")
+    .escape(),
+  body("text")
+    .trim()
+    .isLength({ min: 1 })
+    .withMessage("text must not be empty or undefined")
+    .escape(),
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const comment = new Comment({
+      post: req.params.postid,
+      username: req.body.username,
+      text: req.body.text,
+    });
+
+    await comment.save();
+    return res.status(201).json({ comment });
+  }),
+];
+
+exports.comments_delete = [
+  passport.authenticate("jwt", { session: false }),
+  asyncHandler(async (req, res, next) => {
+    const comment = await Comment.findByIdAndDelete(req.params.commentid);
+    return res.status(200).json({ comment });
+  }),
+];
